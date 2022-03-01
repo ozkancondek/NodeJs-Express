f87b128730c86ea89eb4f4a3128793555a03a530
ozkancondek <ozkncndek@gmail.com>
2022-03-01 16:06:50 +0100

    first commit

---
+++ lesson-1/main.js
@@ -0,0 +1,13 @@
+// //apply node to get output in terminal
+// //cd lesson-1, node main
+// console.log("hello world");
+
+// //js is an asyncron language
+// console.log("first");
+// setTimeout(() => console.log("second"), 1000);
+// console.log("third");
+// //other languages follows the line order but js not
+
+//js is an Event-driven-language
+//requests with promise structure dont include the call stack
+//after they have a response, they wait in callback queue and include the call stack
