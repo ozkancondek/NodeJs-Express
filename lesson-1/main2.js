2e4eb083f17375843ff3365bb7c2dd6c4893d2c9
ozkancondek <ozkncndek@gmail.com>
2022-03-01 17:09:03 +0100

    npm init create project method

---
+++ lesson-1/main2.js
@@ -0,0 +1,5 @@
+//package management
+//npm init (to create npm modules with package.json)
+//give necessary information to your project
+//now i can use npm i package method because its gonna  open a dependencies part in package json automatically
+//e.i.= if i use --save-dev to package installment, its gonna added as a different dependency and only used in develepment phase
