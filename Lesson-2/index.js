f5b38a5be2f944f309d552ad1d1f7d3b606369b5
ozkancondek <ozkncndek@gmail.com>
2022-03-02 13:43:43 +0100

    render a page with view folder

---
+++ Lesson-2/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const path = require("path");
+const UserRouter = require("./routes/UserRouter");
+
+const app = express();
+
+//if there is something in my env use it or 5000
+const port = process.env.port || 5000;
+
+//make arrangement because express need to understand the file rendering and location
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//rewuests
+app.get("/", (req, res) => {
+  //res.send("you connected from home page");
+
+  //show my main.ejs from views when i am in / path
+  res.render("main");
+
+  //i can also send file
+  //   res.sendFile(path.join(__dirname,"textfile.txt"))
+});
+
+//if i have many functions, i need to collect them in route folder
+//so i will dispatch these functions to router
+app.get("/user", UserRouter.get_user);
+
+app.listen(port, () => {
+  console.log("i am listening  ");
+});
---
+++ Lesson-2/routes/UserRouter.js
@@ -0,0 +1,3 @@
+exports.get_user = (req, res) => {
+  res.send("u connected from user path");
+};
