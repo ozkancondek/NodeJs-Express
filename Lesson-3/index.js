b994f3f085002c81dad9d5f7d6f3344093bfbfb1
ozkancondek <ozkncndek@gmail.com>
2022-03-03 23:22:14 +0100

    use logger

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -3,6 +3,9 @@ const express = require("express");
 //set env file, install dotenv package
 require("dotenv").config();
 
+//npm i morgan
+const logger = require("morgan");
+
 //console.log(process.env);  I can see the variables in .env file now
 //normally, process.env is belong to environment of os.
 
@@ -10,6 +13,43 @@ const app = express();
 
 const port = process.env.port || 5000;
 const host = "localhost";
+
+// ********************************************************
+// ********************************************************
+// MIDDLEWARE
+// ********************************************************
+// ********************************************************
+
+// //all process between request and response
+// //u have only two option
+// //u gonna finish process or next middleware
+// //for example there is no return in this function
+// //app.use(() => console.Console.log("middleware:"));
+// app.use((req, res, next) => {
+//   console.Console.log("middleware:1");
+//   //make your duty and go for next
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.Console.log("middleware:2");
+//   //make your duty and go for next
+//   next();
+// });
+// app.use((req, res, next) => {
+//   console.Console.log("middleware:3");
+//   //make your duty and go for next
+//   next();
+// });
+
+//use logger
+app.use(logger());
+app.get("/", (req, res) => {});
+
+// ********************************************************
+// ********************************************************
+// middleware
+// ********************************************************
+// ********************************************************
 
 app.get("/", (req, res) => {
   res.send("hello from / route");
